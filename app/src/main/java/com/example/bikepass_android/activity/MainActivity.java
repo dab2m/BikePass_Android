3d1edf79ef945e7bab35b0e1f0ad637ad98549e8
dilan dogan <d.dogan@etu.edu.tr>
2020-01-18 19:07:27 +0200

    Simple check to get user login data

--- app/src/main/java/com/example/bikepass_android/activity/MainActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/MainActivity.java
@@ -2,11 +2,25 @@ package com.example.bikepass_android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.bikepass_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public void loginInput(View view){
+
+     EditText username=findViewById(R.id.username);
+     EditText password=findViewById(R.id.password);
+
+     Log.i("info","username:"+username.getText().toString());
+     Log.i("info","password:"+password.getText().toString());
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
