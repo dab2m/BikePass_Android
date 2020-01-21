eb65b145ab5091ac5757abddade0856d7cc67291
mustafatozluoglu <m_saidt@hotmail.com>
2020-01-22 00:45:17 +0300

    Added QR code scanner for bike locker

--- app/src/main/java/com/example/bikepass_android/activity/RentBikeActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/RentBikeActivity.java
@@ -1,24 +1,95 @@
 package com.example.bikepass_android.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.bikepass_android.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class RentBikeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView textViewQR;
     Button buttonCancel;
+    SurfaceView surfaceView;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_bike);
 
+        textViewQR = (TextView) findViewById(R.id.textViewQR);
+
         buttonCancel = (Button) findViewById(R.id.buttonCancel);
         buttonCancel.setOnClickListener(this);
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+
+        barcodeDetector = new BarcodeDetector.Builder(this).
+                setBarcodeFormats(Barcode.QR_CODE).build();
+
+        cameraSource = new CameraSource.Builder(this,barcodeDetector).
+                setRequestedPreviewSize(640,480).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    cameraSource.start(surfaceHolder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if(qrCodes.size() != 0){
+                    textViewQR.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(1000);
+                            textViewQR.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
