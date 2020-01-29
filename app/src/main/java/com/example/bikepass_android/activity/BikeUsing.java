1cd4bfbae8f95eeeff206bf009eebd2c4e057813
mustafatozluoglu <m_saidt@hotmail.com>
2020-01-30 00:47:09 +0300

    Added chronometer for bike using time

--- app/src/main/java/com/example/bikepass_android/activity/BikeUsing.java
+++ app/src/main/java/com/example/bikepass_android/activity/BikeUsing.java
@@ -2,14 +2,25 @@ package com.example.bikepass_android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
 
 import com.example.bikepass_android.R;
 
 public class BikeUsing extends AppCompatActivity {
 
+    private Chronometer chronometer;
+    private boolean running;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bike_using);
+
+        chronometer = findViewById(R.id.chronometer);
+    }
+
+    public void startChronometer(View v){
+
     }
 }
