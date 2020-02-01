d1dd5b633d463588bee88a4e777ffa51337b00f5
dilan dogan <d.dogan@etu.edu.tr>
2020-02-02 01:49:25 +0200

    Customizing first location and marker for our app

--- app/src/main/java/com/example/bikepass_android/activity/MapActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/MapActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.bikepass_android.R;
@@ -40,8 +41,9 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng tobb = new LatLng(39.92102,32.797466 );
+
+        mMap.addMarker(new MarkerOptions().position(tobb).title("Marker in Tobb Üniversity").icon(BitmapDescriptorFactory.fromResource(R.drawable.bikelogo)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tobb));
     }
 }
