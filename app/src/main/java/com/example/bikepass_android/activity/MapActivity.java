a469ec59587e621f4d55b3a09262c8b4e47febec
dilan dogan <d.dogan@etu.edu.tr>
2020-02-02 20:21:32 +0200

    Geting user's location if user already gived permission

--- app/src/main/java/com/example/bikepass_android/activity/MapActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/MapActivity.java
@@ -42,22 +42,32 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
     }
+
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                LatLng userLoc = new LatLng(location.getLatitude(),location.getLongitude() );
+                LatLng userLoc = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLoc).title("You are here"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc,15));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc, 15));
                 Toast.makeText(getApplicationContext(), location.toString(), Toast.LENGTH_SHORT).show();
             }
 
@@ -76,48 +86,33 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
             }
         };
+
         //If device is running SDK<23
         if (Build.VERSION.SDK_INT < 23) {
+
+            //Any change of location will be made awere of
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 return;
             }
-            //Any change of location will be made awere of
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
         } else {
             //If we didnt get users permission
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
                 //ask for permission
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             } else {
-
                 //we have permission
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                Location lastKnownLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                LatLng userLoc = new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude() );
+                mMap.addMarker(new MarkerOptions().position(userLoc).title("You are here"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc,15));
+
             }
         }
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng tobb = new LatLng(39.92102,32.797466 );
 
