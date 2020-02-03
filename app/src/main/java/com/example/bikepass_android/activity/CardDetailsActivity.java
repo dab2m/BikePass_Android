04edd212434fa80fd03408211a3454d382669c5f
berksigin <asigin@etu.edu.tr>
2020-02-03 06:27:45 +0300

    Credit Card Details Class Added

---
+++ app/src/main/java/com/example/bikepass_android/activity/CardDetailsActivity.java
@@ -0,0 +1,29 @@
+package com.example.bikepass_android.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.bikepass_android.R;
+
+/**
+ * Created by Berk on 03.02.2020.
+ */
+public class CardDetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_details);
+        Intent intent=getIntent();
+        Toast.makeText(getApplicationContext(),"" +intent.getStringExtra("message"), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
