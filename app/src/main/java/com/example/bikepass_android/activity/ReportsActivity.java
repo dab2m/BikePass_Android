825b881fdf0221e0b3a44bcaa51c9a3b2248996a
berksigin <asigin@etu.edu.tr>
2020-02-09 07:11:29 +0300

    Card Details and Reports Improved

--- app/src/main/java/com/example/bikepass_android/activity/ReportsActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/ReportsActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.bikepass_android.R;
@@ -14,7 +14,7 @@ import com.example.bikepass_android.R;
  */
 public class ReportsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button buttonRentBike;
+    ImageButton buttonRentBike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +22,7 @@ public class ReportsActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_reports);
         Intent intent=getIntent();
         Toast.makeText(getApplicationContext(),"" +intent.getStringExtra("message"), Toast.LENGTH_SHORT).show();
-        buttonRentBike = (Button) findViewById(R.id.b2);
+        buttonRentBike = (ImageButton) findViewById(R.id.b2);
         buttonRentBike.setOnClickListener(this);
     }
 
