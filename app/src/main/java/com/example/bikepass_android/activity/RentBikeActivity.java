359b8eb710ca79d0dd6bff0890eaad53bf6d2fea
mustafatozluoglu <m_saidt@hotmail.com>
2020-03-02 16:29:57 +0300

    QR Code scanner page totally changed

--- app/src/main/java/com/example/bikepass_android/activity/RentBikeActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/RentBikeActivity.java
@@ -1,135 +1,145 @@
 package com.example.bikepass_android.activity;
 
-import android.Manifest;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Build;
-import android.os.Vibrator;
-import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.example.bikepass_android.R;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.zxing.Result;
 
-import java.io.IOException;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-/**
- * Created by MustafaSaid on 21.01.2020
- */
-public class RentBikeActivity extends AppCompatActivity implements View.OnClickListener {
+import static android.Manifest.permission.CAMERA;
 
+public class RentBikeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
-    TextView textViewQR;
-    Button buttonCancel;
-    SurfaceView surfaceView;
-    BarcodeDetector barcodeDetector;
-    CameraSource cameraSource = null;
-
-    private String bikeId;
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView scannerView;
+    private static int camId = Camera.CameraInfo.CAMERA_FACING_BACK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rent_bike);
 
-        textViewQR = (TextView) findViewById(R.id.textViewQR);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+        int currentApiVersion = Build.VERSION.SDK_INT;
 
-        buttonCancel = (Button) findViewById(R.id.buttonCancel);
-        buttonCancel.setOnClickListener(this);
-
-        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-
-        barcodeDetector = new BarcodeDetector.Builder(this).
-                setBarcodeFormats(Barcode.QR_CODE).build();
-
-        cameraSource = new CameraSource.Builder(this, barcodeDetector).
-                setRequestedPreviewSize(640, 480).build();
-
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                    try {
-                        cameraSource.start(surfaceHolder);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+        if (currentApiVersion >= Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                Toast.makeText(getApplicationContext(), "Permission already granted!", Toast.LENGTH_LONG).show();
+            } else {
+                requestPermission();
             }
+        }
+    }
 
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+    private boolean checkPermission() {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
 
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                cameraSource.stop();
-            }
-        });
-
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
-
-                String insideQRCode = "";
-                if (qrCodes.size() != 0)
-                    insideQRCode = qrCodes.valueAt(0).displayValue;
-
-                if (qrCodes.size() != 0 && insideQRCode.toLowerCase().contains("bike")) {
-                    textViewQR.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                            vibrator.vibrate(1000);
-                            String bikeName = qrCodes.valueAt(0).displayValue;
-                            bikeId = bikeName.substring(bikeName.lastIndexOf(" ") + 1);
-
-                            /**
-                             * Bu kod parcasi bikeId'yi BikeUsingActivity'e gecirmek icin yazildi.
-                             */
-                            Intent intent2 = new Intent(RentBikeActivity.this, BikeUsingActivity.class);
-                            intent2.putExtra("key", bikeId);
-                            startActivity(intent2);
-
-
-                        }
-                    });
-                } else {
-                    //Toast.makeText(RentBikeActivity.this.getApplicationContext(), "Invalid QR CODE!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.buttonCancel:
-                startActivity(new Intent(RentBikeActivity.this, BikeUsingActivity.class));
-                //this.finish();
+    public void onResume() {
+        super.onResume();
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                if (scannerView == null) {
+                    scannerView = new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        scannerView.stopCamera();
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0) {
+
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access camera", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and camera", Toast.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(CAMERA)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{CAMERA}, REQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+                    }
+                }
                 break;
         }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.support.v7.app.AlertDialog.Builder(RentBikeActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        final String myResult = result.getText();
+        Log.d("QRCodeScanner", result.getText());
+        Log.d("QRCodeScanner", result.getBarcodeFormat().toString());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("BIKE INFORMATION");
+        builder.setPositiveButton("START", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String bikeId;
+                bikeId = myResult.substring(myResult.lastIndexOf(" ") + 1);
+                /**
+                 * Bu kod parcasi bikeId'yi BikeUsingActivity'e gecirmek icin yazildi.
+                 */
+                Intent intent = new Intent(RentBikeActivity.this, BikeUsingActivity.class);
+                intent.putExtra("key", bikeId);
+                startActivity(intent);
+            }
+        });
+
+        builder.setMessage(result.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
     }
 }
