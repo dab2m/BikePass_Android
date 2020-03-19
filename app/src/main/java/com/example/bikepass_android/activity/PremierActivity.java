fce17bf03187208ea255633a859126e1d79d83e4
dilan dogan <d.dogan@etu.edu.tr>
2020-03-19 18:31:20 +0200

    If user loged for once,dont redirect user to premier activity another time

--- app/src/main/java/com/example/bikepass_android/activity/PremierActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/PremierActivity.java
@@ -1,7 +1,9 @@
 package com.example.bikepass_android.activity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
@@ -15,7 +17,6 @@ import com.example.bikepass_android.R;
  * Created by Mustafa on 10.02.2020
  */
 public class PremierActivity extends AppCompatActivity {
-
     private static int CAMERA_REQUEST_CODE = 100;
 
 
@@ -24,6 +25,15 @@ public class PremierActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_premier);
+
+        SharedPreferences sharedpreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        if (!sharedpreferences.getString("username", "").equals("")){
+            //has login
+            startActivity(new Intent(this, ReportsActivity.class));
+            //must finish this activity (the premier activity will not be shown when click back in reports activity)
+            finish();
+        }
+
 
         // Request for camera permission from user when app is starting
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
