e0a0063b16a29e3a6b964bb12c1cecad1cdc8048
dilan dogan <d.dogan@etu.edu.tr>
2020-03-22 02:14:13 +0200

    location name of the particular latitude and longitude

--- app/src/main/java/com/example/bikepass_android/activity/MapActivity.java
+++ app/src/main/java/com/example/bikepass_android/activity/MapActivity.java
@@ -5,6 +5,8 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -44,6 +46,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -58,6 +62,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     private GoogleMap mMap;
     private boolean isGPS = false;
     private boolean isContinue = false;
+
 
 
     @SuppressLint("MissingPermission")
@@ -93,8 +98,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
+        
         if (!isGPS) {
             Toast.makeText(this, "Please turn on GPS", Toast.LENGTH_SHORT).show();
             return;
@@ -151,6 +155,32 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
 
     }
+
+    private String getAddress(double lat, double lng) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            Address obj = addresses.get(0);
+            String add = obj.getAddressLine(0);
+           // add = add + "\n" + obj.getCountryName();
+            //add = add + "\n" + obj.getCountryCode();
+            //add = add + "\n" + obj.getAdminArea();
+            //add = add + "\n" + obj.getPostalCode();
+            //add = add + "\n" + obj.getSubAdminArea();
+            //add = add + "\n" + obj.getLocality();
+            //add = add + "\n" + obj.getSubThoroughfare();
+            //Log.i("IGA", "getAdminArea:  " + obj.getAdminArea()+ "Submın: "+obj.getSubAdminArea()+ "locality: "+obj.getLocality()+"subtrough "+ obj.getSubThoroughfare());
+            //Toast.makeText(getApplicationContext(), "Address=>" + add, Toast.LENGTH_SHORT).show();
+            return  add;
+            // TennisAppActivity.showDialog(add);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
+        }
+      return null;
+    }
+
     private void getLocation(){
         if (ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -307,6 +337,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
             return null;
         }
+
+
 
 
     }
