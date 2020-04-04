f2c5b32af966e3f05b148401f4b952beea3a22a9
dilan dogan <d.dogan@etu.edu.tr>
2020-04-04 16:01:11 +0200

    Setter methods for Bike

--- app/src/main/java/com/example/bikepass_android/activity/Bike.java
+++ app/src/main/java/com/example/bikepass_android/activity/Bike.java
@@ -16,9 +16,15 @@ package com.example.bikepass_android.activity;
 
     public int getStatus_code(){return this.status_code;}
 
+    public void setStatus_code(int status_code){this.status_code=status_code;}
+
     public String getStatus_name(){return this.status_name;}
+
+     public void setStatus_name(String status_name){this.status_name=status_name;}
 
     public int getLogo_name(){return this.logo_name;}
 
-     public int getId(){return this.id;}
+    public void setLogo_name(int logo_name){this.logo_name=logo_name;}
+    
+    public int getId(){return this.id;}
 }
