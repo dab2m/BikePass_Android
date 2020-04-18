0b9f59b0b6ae135308c0b753940a51f0e16f1df3
dilan dogan <d.dogan@etu.edu.tr>
2020-04-18 15:32:17 +0200

    Taking image of damaged bike

--- app/src/main/java/com/example/bikepass_android/activity/ReportBikeDamage.java
+++ app/src/main/java/com/example/bikepass_android/activity/ReportBikeDamage.java
@@ -2,11 +2,14 @@ package com.example.bikepass_android.activity;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
@@ -28,7 +31,10 @@ public class ReportBikeDamage extends AppCompatActivity {
     TextView clickedText;
     private static final int CAMERA_REQUEST = 1888;
     ImageView imageView;
+    Uri image_uri;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final int PERMISSION_CODE=1000;
+    private static final int IMAGE_CAPTURE_CODE=1001;
     LinearLayout photoButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,18 +55,40 @@ public class ReportBikeDamage extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v)
-            {
-                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {//If system os is >=marshmallow ,request runtime permission
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED) {
+                        //Permission not enabled,request it
+
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //Showpopuptorequest permissions
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {
+                        //permission already granted
+                        openCamera();
+                    }
+                }
+                    else{
+                        //systemos<marshmallow
+                        openCamera();
+                    }
+
+               // startActivity(new Intent(ReportBikeDamage.this,PhotoActivity.class));
+              /*  if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
                 {
                     requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
                 }
                 else
                 {
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                }
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 0);
+                } */
             }
         });
+        imageView=findViewById(R.id.putphoto);
         ImageView arrow=findViewById(R.id.txtclose);
         arrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +106,56 @@ public class ReportBikeDamage extends AppCompatActivity {
                 startActivity(new Intent(ReportBikeDamage.this, ScanQrCode.class));
             }
         });
+
+       setListenersForButtons();
+    }
+
+    public void openCamera(){
+
+        ContentValues values=new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"New Pıcture");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From the Camera");
+        image_uri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        //Camera intent
+        Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE);
+
+    }
+
+    //handling permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+       //this method is called,when user presses Aloow or Deny from permission request poup
+        switch(requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    openCamera();
+                }
+                else{
+                    Toast.makeText(this,"Permission denied..",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == RESULT_OK )
+        {
+            //Bitmap photo = (Bitmap) data.getExtras().get("data");
+            TextView damagephoto=findViewById(R.id.damagephoto);
+            damagephoto.setVisibility(View.INVISIBLE);
+            //imageView.setImageBitmap(photo);
+            imageView.setImageURI(image_uri);
+            //photoButton.addView(imageView);
+        }
+    }
+
+    public void setListenersForButtons(){
 
         final LinearLayout qrcodelayout=findViewById(R.id.qrcodelayout);
         final TextView qrcodetext=findViewById(R.id.qrcodetext);
@@ -129,7 +207,7 @@ public class ReportBikeDamage extends AppCompatActivity {
             }
         });
     }
-    @Override
+ /*   @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
     {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -139,7 +217,7 @@ public class ReportBikeDamage extends AppCompatActivity {
             {
                 Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                startActivityForResult(cameraIntent, 0);
             }
             else
             {
@@ -157,9 +235,9 @@ public class ReportBikeDamage extends AppCompatActivity {
             TextView damagephoto=findViewById(R.id.damagephoto);
             damagephoto.setVisibility(View.INVISIBLE);
             imageView.setImageBitmap(photo);
-            photoButton.addView(imageView);
+            //photoButton.addView(imageView);
         }
-    }
+    }  */
     @Override
     public void onBackPressed() {
 
